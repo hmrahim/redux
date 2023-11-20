f09c47245ed22fce21910f2d97db337e60b3a70c
hmrahim <h.m.rahimnet@gmail.com>
2023-11-20 12:08:57 +0600

    increment counter by payload value

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const { createStore } = require("redux");
 
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const RESET = "RESET"
+const INCREMENT_BY_VALUE = "INCREMENT_BY_VALUE";
+const DECREMENT_BY_VALUE = "DECREMENT_BY_VALUE";
+
+const RESET = "RESET";
 // inital state
 const initialState = {
   count: 0,
@@ -20,12 +23,24 @@ const incrementCounter = () => {
 const deccrementCounter = () => {
   return { type: DECREMENT };
 };
+const incrementByValue = (value) => {
+  return {
+    type: INCREMENT_BY_VALUE,
+    payload: value,
+  };
+};
+const decrementByValue = (value) => {
+  return {
+    type: DECREMENT_BY_VALUE,
+    payload: value,
+  };
+};
 
-const resetCounter= ()=> {
-    return {
-        type:RESET
-    }
-}
+const resetCounter = () => {
+  return {
+    type: RESET,
+  };
+};
 // REDUCER FUNCTION
 
 const counterReducer = (state = initialState, action) => {
@@ -40,27 +55,39 @@ const counterReducer = (state = initialState, action) => {
         ...state,
         count: state.count - 1,
       };
-      case RESET:
-        return {
-            ...state,
-            count:0
-        }
+    case INCREMENT_BY_VALUE:
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+    case DECREMENT_BY_VALUE:
+      return {
+        ...state,
+        count: state.count - action.payload,
+      };
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
 
     default:
       state;
   }
 };
 
-// create store 
-const store = createStore(counterReducer)
+// create store
+const store = createStore(counterReducer);
 
-store.subscribe(()=> {
-    console.log(store.getState());
-})
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(incrementCounter())
-store.dispatch(deccrementCounter())
-store.dispatch(resetCounter())
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+
+store.dispatch(incrementByValue(10))
+store.dispatch(incrementByValue(6))
+store.dispatch(decrementByValue(5))
